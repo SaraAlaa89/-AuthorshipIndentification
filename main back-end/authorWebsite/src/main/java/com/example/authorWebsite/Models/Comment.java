875bb470d831fe6e9ddb6a15cfea4a6506ab6810
SaraAlaa89 <76518903+SaraAlaa89@users.noreply.github.com>
2023-06-10 17:28:53 +0300

    main

---
+++ main back-end/authorWebsite/src/main/java/com/example/authorWebsite/Models/Comment.java
@@ -0,0 +1,40 @@
+package com.example.authorWebsite.Models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NonNull
+    private String content;
+
+    @JsonIgnore
+    private Date date;
+
+    @Column(name = "article_id")
+    private int article_id;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id",nullable = false,insertable = false,updatable = false)
+    @JsonIgnore
+    Article article;
+
+
+}
